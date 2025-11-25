08ec412b417b35f3d8b1aedfc46d92e088129133
james1314520 <2435883077@qq,com>
2025-11-25 09:42:29 +0800

    set device

--- examples/fp8_lighting_indexer.py
+++ examples/fp8_lighting_indexer.py
@@ -8,7 +8,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 import argparse
 
 import torch
-torch.npu.set_device(15)
+torch.npu.set_device(0)
 
 import tilelang
 import tilelang.language as T
